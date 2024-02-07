e0eb456cfc343291df499400e0a48a64b0aac289
rays63 <raymondmhz63@gmail.com>
2024-02-07 17:20:30 +0545

    minor fix

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'home'
 urlpatterns = [
     path('',views.home,name='home'),
-    path('dwn/', views.cheatsheet, name="cheatsheet"),
+    path('download/', views.cheatsheet, name="cheatsheet"),
     path('download/<int:cheatsheet_id>/',
          views.download_cheatsheet, name='download_cheatsheet'),
 ]
