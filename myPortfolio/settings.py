c6ee67c99cedbdeacb126975738ad76370fe7cf8
rays63 <57377566+rays63@users.noreply.github.com>
2025-06-09 09:18:45 +0545

    Integrate Git API to Dynamically Display Repository Information in Portfolio (#6)
    * refactor: removed unnecessary commit and file
    
    * feat: add feature to fetch git repo
    
    * chore: refactored the resume and implemented new ui
    
    ---------
    
    Co-authored-by: Raymond Maharjan <raymond.maharjan@bajratechnologies.com>

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
